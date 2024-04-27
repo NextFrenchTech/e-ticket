e03847acc448ae96dee0a44fce69d75eff9c18dd
NextFrenchTech <ikiboo.sys@gmail.com>
2024-04-27 19:10:48 +0200

    remove function

--- js/main.js
+++ js/main.js
@@ -46,3 +46,31 @@ h.to("#app", {
     duration: speed/2,
     ease: "sine.out"
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Sélection du bouton
+    var showFormButton = document.getElementById("showFormButton");
+    
+    // Sélection de la div du formulaire
+    var vismeFormDiv = document.querySelector(".visme_d");
+
+    // Écouteur d'événement pour le clic sur le bouton
+    showFormButton.addEventListener("click", function() {
+        // Affichage du formulaire en changeant sa visibilité
+        vismeFormDiv.style.display = "block";
+    });
+});
+document.addEventListener("DOMContentLoaded", function() {
+    var userNum = document.querySelector(".userNum");
+
+    // Fonction pour cacher le texte du formulaire
+    function hideFormText() {
+        userNum.classList.add("hidden");
+
+        // Retirer l'écouteur d'événement après la première interaction
+        document.removeEventListener("mousemove", hideFormText);
+    }
+
+    // Ajouter un écouteur d'événement pour le déplacement de souris
+    document.addEventListener("mousemove", hideFormText);
+});
